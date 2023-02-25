8ba3cc4293a22c6c592f9d570325b3d5f70a1c12
Victor <victorshliu@gmail.com>
2023-02-25 13:51:22 -0800

    v2

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
 
     <?php require('partials/global/header.php'); ?>
    
-    <div class="container ig">      
+    <div class="container ig"> 
             <?php foreach($player as $tiles): ?>
                 <div class="card">
                     <?php include('partials/template/card.php'); ?>
                 </div>
             <?php endforeach; ?>
     </div>
+    <?php require('partials/global/footer.php'); ?>
 </body>
 <script src="/script.js"></script> 
 </html>
---
+++ partials/global/footer.php
@@ -0,0 +1,3 @@
+<footer>
+  <p>&copy; 2023 Company Name. All Rights Reserved.</p>
+</footer>
